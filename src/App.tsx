5f0ccb6d86de9af40e4a0ddf532a53b786fa8abd
Keven Teles <glevenkeven@gmail.com>
2023-04-11 00:59:48 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <section className='container'>
+        <div className='details'>
+            <span>ADVICE #117</span>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <img src="" alt="" />
+        </div>
+    </section>
+  )
+}
+
+export default App
