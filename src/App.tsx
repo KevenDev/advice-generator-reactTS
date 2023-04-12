fc47a325da9e806c3644aead08e50e4fa72bf48e
Keven Teles <glevenkeven@gmail.com>
2023-04-12 19:02:13 -0300

    feat: new script for dinamic values

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,52 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import dice from './assets/icon-dice.svg'
+import patternMobile from './assets/pattern-divider-mobile.svg'
 
 
 
 function App() {
+  const [data, setData] = useState<responseData>({
+      slip:  {
+          id: 0,
+          advice: ''
+      }
+  });
 
-const [advice, setAdvice] = useState('')
-const getAdvice = () =>{
-  fetch('https://api.adviceslip.com/advice').then(res => res.json()).then(data => console.log(data))
-}
+  interface responseData {
+      slip: {
+          id: number,
+          advice: string,
+      },
+  }
 
+  const handleClick = () =>{
+    fetch('https://api.adviceslip.com/advice').then(res => res.json()).then(data=> setData(data))
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice').then(res => res.json()).then(data=> setData(data))
+  },[])
 
   return (
-    <section className='container'>
-        <div className='details'>
-            <span>ADVICE #117</span>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <div className='images'>
-              <svg className='pattern' width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
-              <div className='dice'> 
-                <button> <svg  width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg> </button> 
-              </div>
-            </div>
-        </div>
-    </section>
+    <div className='app'>
+      <section className='container'>
+              <span>ADVICE #{data.slip.id}</span>
+              <p>{data.slip.advice}</p>
+              <div className='images'>
+                  <img src={patternMobile} alt="" />
+              </div>    
+                <button
+                  className='btn'
+                  onClick={handleClick}> 
+                  <img src={dice} alt="" /> 
+                </button>
+                <footer id='footer'>
+                  <span >Coded By KevenDev</span>
+                </footer>
+      </section>
+    </div>
   )
 }
 
